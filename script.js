e3baa1322155763604cfaddaf4d852b4f5820ef7
FranciscoJ <brunoloouuiis@gmail.com>
2025-08-02 15:36:47 +0300

    feature Players name added

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 'use strict';
 
 // Selecting node elements
+const startGame = document.querySelector('.btnStart');
+const startMain = document.querySelector('.startMain');
+const wrapperHidden = document.querySelector('.wrapper_hidden');
+
+// palyers names
+const playerName0El = document.getElementById('player0');
+const playerName1El = document.getElementById('player1');
+
+const name0El = document.getElementById('name--0');
+const name1El = document.getElementById('name--1');
+
 const playerX = document.querySelector('.playerX');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
@@ -16,7 +27,23 @@ const btnHold = document.querySelector('.btn--hold');
 
 let scores, currentScore, activePlayer, playing;
 
+// Open game function 
+const openG = function () {
+
+    if (!playerName0El.value || !playerName1El.value) return null
+
+    wrapperHidden.classList.add('hidden');
+    startMain.classList.remove('hidden');
+
+    name0El.textContent = playerName0El.value;
+    name1El.textContent = playerName1El.value;
+}
+
+// Start the gane
+startGame.addEventListener('click', openG);
+
 const init = function () {
+
     scores = [0, 0];
     currentScore = 0;
     activePlayer = 0;
@@ -35,18 +62,26 @@ const init = function () {
     player0El.classList.add('player--active');
     player1El.classList.remove('player--active');
 
-    playerX.textContent = `Player ${activePlayer +1}`;
+    playerX.classList.add("hidden");
+
+    // new
+    wrapperHidden.classList.remove('hidden');
+    startMain.classList.add('hidden');
+
+    playerName0El.value = '';
+    playerName1El.value = '';
 
 }
 
 init();
 
-// function Switch Player
+// Function Switch Player
 const switchPlayer = function () {
+
     document.getElementById(`current--${activePlayer}`).textContent = 0;
     currentScore = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
-    playerX.textContent = `Player ${activePlayer +1}`;
+
     player0El.classList.toggle('player--active');
     player1El.classList.toggle('player--active');
 }
@@ -75,18 +110,23 @@ btnRoll.addEventListener('click', function () {
     }
 })
 
+// Holding score
 btnHold.addEventListener('click', function () {
+
     if (playing) {
         // 1. Add current score to active player's score
         scores[activePlayer] += currentScore;
         document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
         // 2. Check if player score is >-100
-        if (scores[activePlayer] >= 10) {
+        if (scores[activePlayer] >= 100) {
+
             // Finish the game
             playing = false;
             diceEl.classList.add('hidden');
-            playerX.textContent = `Player ${activePlayer +1} Wins 🎉`;
+            const winner = document.getElementById(`player${activePlayer}`);
+            playerX.classList.remove("hidden");
+            playerX.textContent = `${winner.value} wins 🎉 `;
 
             document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
             document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
